42282f61cb8d53e176220de629ff014d2055ba95
Tristen <tristenvanbronckhorst@gmail.com>
2025-10-26 16:36:13 +0100

    Fix sitemap generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -56,18 +56,18 @@ export default function Sitemap() {
     },
   ]
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${staticPages
-  .map(
-    (page) => `  <url>
+  const sitemapEntries = staticPages.map((page) => 
+    `  <url>
     <loc>${baseUrl}${page.url}</loc>
     <lastmod>${page.lastModified.toISOString()}</lastmod>
     <changefreq>${page.changeFrequency}</changefreq>
     <priority>${page.priority}</priority>
   </url>`
-  )
-  .join('\n')}
+  ).join('\n')
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${sitemapEntries}
 </urlset>`
 
   return new Response(sitemap, {
