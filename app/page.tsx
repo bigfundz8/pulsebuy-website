1da2d62eacb537ab228749284da785580b55c356
Tristen <tristenvanbronckhorst@gmail.com>
2025-10-26 18:20:40 +0100

    Fix homepage for Vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-'use client'
-
-import { useEffect, useState } from 'react'
 import HomeContentServer from './components/HomeContentServer'
 
 export default function Home() {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    fetch('/api/products')
-      .then(res => res.json())
-      .then(data => setProducts(data.products || []))
-      .catch(err => console.error('Error fetching products:', err))
-  }, [])
-
-  return <HomeContentServer products={products} />
+  return <HomeContentServer products={[]} />
 }
